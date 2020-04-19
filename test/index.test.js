6e1c7fad7f139b5f55e34597265a5cfec3abdcd2
Katie Williams <kawilliams8@gmail.com>
2020-04-19 12:54:44 -0600

    Add hook to spin up db for Messages tests

--- test/index.test.js
+++ test/index.test.js
@@ -1,6 +1,6 @@
 import { expect, server, BASE_URL } from './setup';
 
-describe('Index page test', () => {
+describe('Index', () => {
   it('gets base url', (done) => {
     server
       .get(`${BASE_URL}/`)
@@ -15,7 +15,7 @@ describe('Index page test', () => {
   });
 });
 
-describe('About page test', () => {
+describe('About', () => {
   it('gets about url', (done) => {
     server
       .get(`${BASE_URL}/about`)
@@ -28,20 +28,39 @@ describe('About page test', () => {
   });
 });
 
-// describe('Messages page test', () => {
-//   const mockResponse = [
-//     { name: 'chidimo', message: 'first message' },
-//     { name: 'orji', message: 'second message' },
-//   ];
+describe('Messages page test', () => {
+});
 
-//   it('gets Messages url', (done) => {
-//     server
-//       .get(`${BASE_URL}/messages`)
-//       .expect(200)
-//       .end((err, res) => {
-//         expect(res.status).to.equal(200);
-//         expect(res.body.message).to.equal(mockResponse);
-//         done();
-//       });
-//   });
-// });
+describe('Messages', () => {
+  const mockMessages = [
+    { name: 'chidimo', message: 'first message' },
+    { name: 'orji', message: 'second message' },
+  ];
+  //My test
+  it('gets messages page 1', (done) => {
+    server
+      .get(`${BASE_URL}/messages`)
+      .expect(200)
+      .end((err, res) => {
+        expect(res.status).to.equal(200);
+        expect(res.body.messages).to.deep.equal(mockMessages);
+        done();
+      });
+  });
+
+  //Tutorial test
+  it('get messages page 2', (done) => {
+    server
+      .get(`${BASE_URL}/messages`)
+      .expect(200)
+      .end((err, res) => {
+        expect(res.status).to.equal(200);
+        expect(res.body.messages).to.be.instanceOf(Array);
+        res.body.messages.forEach((m) => {
+          expect(m).to.have.property('name');
+          expect(m).to.have.property('message');
+        });
+        done();
+      });
+  });
+});
