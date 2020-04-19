e8da00850432afe220c2841cd485d76999635d1d
Katie Williams <kawilliams8@gmail.com>
2020-04-19 14:00:06 -0600

    Add POST capabilities, test coverage

--- src/routes/index.js
+++ src/routes/index.js
@@ -1,9 +1,15 @@
 import express from 'express';
-import { indexPage, aboutPage, messagesPage } from '../controllers';
+import {
+  indexPage,
+  aboutPage,
+  messagesPage,
+  addMessage
+} from '../controllers';
 
 const indexRouter = express.Router();
 indexRouter.get('/', indexPage);
 indexRouter.get('/about', aboutPage);
 indexRouter.get('/messages', messagesPage);
+indexRouter.post('/messages', addMessage);
 
 export default indexRouter;
